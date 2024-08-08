acebb30fdfa8e8511518acaff8af52cc0d087374
Selin <selin_cansu@hotmail.com>
2024-08-09 02:02:43 +0300

    ignore

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(process.cwd(), 'src/app/styles')],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
